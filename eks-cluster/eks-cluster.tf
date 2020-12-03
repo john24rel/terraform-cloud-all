e60e81d0ea447c283e72beb95ea87e4eb6e1564e
john24rel <72477831+john24rel@users.noreply.github.com>
2020-12-03 15:19:51 -0600

    Update eks-cluster.tf

--- eks-cluster/eks-cluster.tf
+++ eks-cluster/eks-cluster.tf
@@ -1,8 +1,34 @@
-region = "us-east-2"
-cluster_name = "my-cluster"
-cluster_version = "1.14"
-subnets = ["subnet-0ef4a790d04655f17", "subnet-042f3148fa63ea57e", "subnet-0c1faf9f9e20d6aaa"]
-vpc_id = "vpc-0a6f0eb80882caea8"
-instance_type = "m4.large"
-asg_max_size = 5
-        asg_min_size = 3
+data "aws_eks_cluster" "cluster" {
+  name = module.my-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.my-cluster.cluster_id
+}
+
+provider "kubernetes" {
+  host = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token = data.aws_eks_cluster_auth.cluster.token
+  load_config_file = false
+  version = "~> 1.9"
+}
+
+module "my-cluster" {
+  source = "terraform-aws-modules/eks/aws"
+  version = "12.0.0"
+  region = "us-east-2"
+  cluster_name = "my-cluster"
+  cluster_version = "1.14"
+  subnets = ["subnet-0ef4a790d04655f17", "subnet-042f3148fa63ea57e", "subnet-0c1faf9f9e20d6aaa"]
+  vpc_id = "vpc-0a6f0eb80882caea8"
+  instance_type = "m4.large"
+  asg_max_size = "5"
+  asg_min_size = "3"
+  worker_groups = [{
+    instance_type = "${var.instance_type}"
+    asg_max_size = "${var.asg_max_size}"
+	asg_min_size = "${var.asg_min_size}"
+    }
+  ]
+}

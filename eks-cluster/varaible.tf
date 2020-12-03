867fab74fd7f6ea3e22212f7a6b54d9ae8516f59
john24rel <72477831+john24rel@users.noreply.github.com>
2020-12-03 16:06:44 -0600

    Update varaible.tf

--- eks-cluster/varaible.tf
+++ eks-cluster/varaible.tf
@@ -1,8 +1,9 @@
-region = us-east-2
-cluster_name = my-cluster
-cluster_version = 1.14
-subnets = [subnet-0ef4a790d04655f17, subnet-042f3148fa63ea57e, subnet-0c1faf9f9e20d6aaa]
-vpc_id = vpc-0a6f0eb80882caea8
-instance_type = m4.large
-asg_max_size = 5
-asg_min_size =3
+variable cluster_name {} 
+variable cluster_version {}
+variable subnets {
+    type = list
+}
+variable vpc_id {}
+variable instance_type {}
+variable asg_max_size {}
+variable asg_min_size {}

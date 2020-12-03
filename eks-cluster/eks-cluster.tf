55e32948f1288ee83d7167663434c9f0caca17a5
john24rel <72477831+john24rel@users.noreply.github.com>
2020-12-03 15:28:17 -0600

    Update eks-cluster.tf

--- eks-cluster/eks-cluster.tf
+++ eks-cluster/eks-cluster.tf
@@ -23,12 +23,9 @@ module "my-cluster" {
   subnets = ["subnet-0ef4a790d04655f17", "subnet-042f3148fa63ea57e", "subnet-0c1faf9f9e20d6aaa"]
   vpc_id = "vpc-0a6f0eb80882caea8"
   instance_type = "m4.large"
+  worker_groups = [{
   asg_max_size = "5"
   asg_min_size = "3"
-  worker_groups = [{
-    instance_type = "${var.instance_type}"
-    asg_max_size = "${var.asg_max_size}"
-	asg_min_size = "${var.asg_min_size}"
-    }
+  }
   ]
 }
